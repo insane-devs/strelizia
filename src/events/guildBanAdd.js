3d10d947c742b8f1e1d145ab6dfd98e4f820e0c3
TheFloppyBanana <35372554+TheFloppyBanana@users.noreply.github.com>
2020-10-27 09:53:47 +0800

    feat(bans): track manual bans made by mods

---
+++ src/events/guildBanAdd.js
@@ -0,0 +1,27 @@
+const { MessageEmbed } = require('discord.js');
+const { Event, Duration } = require('klasa');
+
+module.exports = class extends Event {
+
+	async run(guild, user) {
+		if (guild.id !== '508495069914071040') return;
+
+		const auditData = (await guild.fetchAuditLogs({
+			type: 22
+		})).entries.find(data => data.target === user);
+		if (!auditData) return;
+		const { executor, reason } = auditData;
+
+		if (executor.bot) return;
+		await this.client.channels.get('573122270646501376').send(new MessageEmbed()
+			.setAuthor('🚨 Member Banned', user.displayAvatarURL())
+			.setDescription(`**ID**: ${user.id}`)
+			.addField('User', user.tag)
+			.addField('Banned by:', executor.tag, true)
+			.addField('Reason', reason || 'None', true)
+			.setColor('RED')
+			.setFooter(`Joined Discord: ${Duration.toNow(user.createdTimestamp)} ago`)
+			.setTimestamp()).catch(console.error);
+	}
+
+};
