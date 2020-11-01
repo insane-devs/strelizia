ea6efd9fd824479858c8cc3a28cbf87313ab252c
TheFloppyBanana <35372554+TheFloppyBanana@users.noreply.github.com>
2020-11-01 13:30:15 +0800

    src: config changes

--- src/app.js
+++ src/app.js
@@ -2,23 +2,28 @@ const HayasakaClient = require('./lib/structures/HayasakaClient');
 const { token, prefix } = require('../config');
 
 new HayasakaClient({
-	fetchAllMembers: false,
-	prefix: prefix,
 	commandEditing: true,
 	commandLogging: true,
+	console: {
+		useColor: true
+	},
+	consoleEvents: {
+		verbose: true
+	},
+	fetchAllMembers: false,
+	messageCacheMaxSize: 30,
+	messageCacheLifetime: 300,
+	messageSweepInterval: 600,
+	restSweepInterval: 60,
+	restTimeOffset: 100,
 	owners: ['296862433136476160', '295391820744228867'],
 	pieceDefaults: {
 		commands: {
 			deletable: true
 		}
 	},
-	consoleEvents: {
-		verbose: true
-	},
-	console: {
-		useColor: true
-	},
+	prefix: prefix,
+	readyMessage: (client) => `Successfully initialized. Ready to serve ${client.guilds.size} guilds.`,
 	regexPrefix: /^((?:Hey )?(Haya|Hayasaka)(?:,|!| ))/i,
-	typing: true,
-	readyMessage: (client) => `Successfully initialized. Ready to serve ${client.guilds.size} guilds.`
+	typing: true
 }).login(token);
