6a50c59f4e3c0b0cd999e89d61dd4c1f6e67f941
TheFloppyBanana <35372554+TheFloppyBanana@users.noreply.github.com>
2021-02-06 13:39:52 +0800

    feat(watchdog): sort members who left/was banned in a different field

--- src/events/watchdog.js
+++ src/events/watchdog.js
@@ -9,6 +9,15 @@ module.exports = class extends Event {
 
 		if (!(channel || textChannel)) return;
 
+		watchlist.forEach(async ({ user }) => {
+			await message.guild.members.fetch(user)
+				.catch(async () => {
+					const entry = watchlist.find(data => data.user === user.id);
+					entry.left = true;
+					await message.guild.settings.update('watchlist', entry, { guild: message.guild, arrayIndex: watchlist.indexOf(entry) });
+				});
+		});
+
 		try {
 			const msg = await textChannel.messages.fetch(id);
 			await msg.edit(this.buildEmbed(message, watchlist));
@@ -19,20 +28,28 @@ module.exports = class extends Event {
 	}
 
 	buildEmbed(message, watchlistData) {
-		return new MessageEmbed()
+		const embed = new MessageEmbed()
 			.setAuthor(`${message.guild} watchlist`, message.guild.iconURL({ dynamic: true }))
-			.addField('<:dnd:415894324522254338> High Priority', this._filterByPriority(watchlistData, 'high'))
-			.addField('<:idle:415894324610596865> Medium Priority', this._filterByPriority(watchlistData, 'medium'))
-			.addField('<:online:415894324652277762> Low Priority', this._filterByPriority(watchlistData, 'low'))
+			.addField('<:dnd:415894324522254338> **High Priority**', this._filterByPriority(watchlistData, 'high'))
+			.addField('<:idle:415894324610596865> **Medium Priority**', this._filterByPriority(watchlistData, 'medium'))
+			.addField('<:online:415894324652277762> **Low Priority**', this._filterByPriority(watchlistData, 'low'))
 			.setColor('RED')
 			.setTimestamp();
+		if (this._filterLeftMembers(watchlistData).length) embed.addField('**Members who left/were banned**', this._filterLeftMembers(watchlistData).join('\n'));
+		return embed;
 	}
 
 	_filterByPriority(array, priority) {
-		const filtered = array.filter((data) => data.priority === priority);
+		const filtered = array.filter((data) => !data.left && data.priority === priority);
 		return filtered.length ?
 			filtered.map(({ user, reason, moderator }) => `• <@${user}> ─ ${reason} (<@${moderator}>)`).join('\n') :
 			'Empty';
+	}
+
+	_filterLeftMembers(array) {
+		return array
+			.filter(data => data.left)
+			.map(({ user, reason, moderator }) => `• <@${user}> ─ ${reason} (<@${moderator}>)`);
 	}
 
 };
