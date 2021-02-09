0a167dbf99302cfa1859076d6137165294b71f09
TheFloppyBanana <35372554+TheFloppyBanana@users.noreply.github.com>
2021-02-09 10:54:34 +0800

    add: constants file

---
+++ src/lib/util/constants.js
@@ -0,0 +1,16 @@
+exports.EMOTES = {
+	check: '<:checkmark:415894323436191755>',
+	cross: '<:xmark:415894324719386634>',
+	typing: '<a:typing:492356308943503370>'
+};
+
+exports.ANSWERS = [
+	'Yes',
+	'No',
+	'I doubt it.',
+	"I don't think so..",
+	"It's a yes",
+	'Perhaps',
+	'Never',
+	'Yep'
+];
