6ba63c54dcc19bdf569b502003b505242b85ec10
TheFloppyBanana <35372554+TheFloppyBanana@users.noreply.github.com>
2021-02-09 10:57:06 +0800

    refactor(musicLeave): use id instead of guild object

--- src/tasks/musicLeave.js
+++ src/tasks/musicLeave.js
@@ -6,8 +6,8 @@ module.exports = class extends Task {
 		super(...args, { enabled: true });
 	}
 
-	async run({ guild }) {
-		const { music } = guild;
+	async run({ guildID }) {
+		const { music } = this.client.guilds.cache.get(guildID);
 		if (music.queue.length) return null;
 		if (music.textChannel) await music.textChannel.send('I have left the voice channel because of inactivity and to save resources.');
 		return music.destroy();
