a5bd23c8cf5214374e2acfdff8313b30b8e28af4
TheFloppyBanana <35372554+TheFloppyBanana@users.noreply.github.com>
2021-04-01 14:35:48 +0800

    misc(GuildSchema): add appeals piece

--- src/lib/schemas/GuildSchema.js
+++ src/lib/schemas/GuildSchema.js
@@ -5,7 +5,10 @@ Client.defaultGuildSchema
 	.add('prefix', 'string', { default: prefix })
 	.add('raidmode', 'boolean', { default: false })
 	.add('watchlist', 'any', { default: [], array: true })
-	.add('appeals', 'any', { default: [], array: true })
+	.add('appeals', folder => folder
+		.add('list', 'any', { default: [], array: true })
+		.add('channel', 'textchannel', { default: null })
+		.add('messageID', 'string', { default: null }))
 	.add('watchdog', folder => folder
-		.add('channel', 'textchannel', { defaut: null })
+		.add('channel', 'textchannel', { default: null })
 		.add('messageID', 'string', { default: null }));
