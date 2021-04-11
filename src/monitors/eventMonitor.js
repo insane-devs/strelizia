0ad7fda20de4927270c8c21fb80124d4e30d01f4
TheFloppyBanana <35372554+TheFloppyBanana@users.noreply.github.com>
2021-04-11 14:51:06 +0800

    fix(eventMonitor): wrong channel id

--- src/monitors/eventMonitor.js
+++ src/monitors/eventMonitor.js
@@ -23,7 +23,7 @@ module.exports = class extends Monitor {
 	async run(message) {
 		if (!message.guild ||
 			message.guild.id !== '508495069914071040' ||
-			message.channel.id !== '534138604281266195') return null;
+			message.channel.id !== '815308859580874762') return null;
 		if (!new RegExp(days.join('|')).test(message.content)) return null;
 		if (/\b(nsfw|dick|pussy|naked)\b|(loli|tit)/.test(message.content)) return message.react(no);
 		for (const emote of REACTIONS) {
