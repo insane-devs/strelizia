a09ae0c118125ec30493569b13a001244c1ae775
TheFloppyBanana <35372554+TheFloppyBanana@users.noreply.github.com>
2021-05-16 15:12:50 +0800

    refactor(topPosters): store message IDs and number of images posted per user in an object

--- src/tasks/topPosters.js
+++ src/tasks/topPosters.js
@@ -2,50 +2,80 @@ const { Task, util: { codeBlock } } = require('klasa');
 
 module.exports = class extends Task {
 
+	constructor(...args) {
+		super(...args);
+		this.lb = {
+			eventID: '',
+			lastID: '',
+			messages: [],
+			posters: {},
+			totalImages: 0
+		};
+	}
+
 	async run({ guild, force = false }) {
 		if (!this.client.settings.get('eventID')) return null;
+
 		const _guild = this.client.guilds.cache.get(guild);
 		const eventChannel = _guild.channels.cache.get('534138604281266195');
-		const messages = [];
-		const obj = {};
-		const options = { limit: 100 };
+		const eventID = this.client.settings.get('eventID');
 		const lastPostID = this.client.settings.get('lastLeaderboardPost');
-		let lastID = this.client.settings.get('eventID');
-		let totalImages = 0;
+
+		if (this.lb.eventID && this.lb.eventID !== eventID) {
+			this._reset();
+		} else {
+			this.lb.eventID = eventID;
+		}
+
+		const options = { limit: 100 };
 
 		// eslint-disable-next-line no-constant-condition
 		while (true) {
 			try {
-				if (lastID) options.after = lastID;
+				options.after = this.lb.lastID || eventID;
 				const msgs = await eventChannel.messages.fetch(options, false);
-				messages.push(...msgs.array());
-				lastID = msgs.first().id;
+				this.lb.messages.push(...msgs.array());
+				this.lb.lastID = msgs.first().id;
 			} catch (err) { break; }
 		}
 
-		messages.push(await eventChannel.messages.fetch(this.client.settings.get('eventID'), false));
+		this.lb.messages.push(await eventChannel.messages.fetch(this.client.settings.get('eventID'), false));
 
-		const index = messages.sort((a, b) => b.createdTimestamp - a.createdTimestamp).findIndex(msg => msg.id === lastPostID);
+		const index = this.lb.messages.sort((a, b) => b.createdTimestamp - a.createdTimestamp).findIndex(msg => msg.id === lastPostID);
 
 		if (!force && index < 50 && index !== -1) return null;
 
-		messages.filter(msg => msg.attachments.size).forEach(msgs => {
-			totalImages += msgs.attachments.size;
-			if (!obj[msgs.author.tag]) obj[msgs.author.tag] = msgs.attachments.size;
-			else obj[msgs.author.tag] += msgs.attachments.size;
+		this.lb.messages.filter(msg => msg.attachments.size).forEach(msgs => {
+			this.lb.totalImages += msgs.attachments.size;
+			if (!this.lb.posters[msgs.author.id]) this.lb.posters[msgs.author.id] = msgs.attachments.size;
+			else this.lb.posters[msgs.author.id] += msgs.attachments.size;
 		});
 
-		const sorted = Object.entries(obj).filter(ent => ent[1] >= 10).sort((a, b) => b[1] - a[1]).map(data => `• ${data[0]}${''.padStart(40 - data[0].length, ' ')}:: ${data[1]} images`);
+		const sorted = await Promise.all(Object.entries(this.lb.posters).filter(ent => ent[1] >= 15).sort((a, b) => b[1] - a[1]).map(async data => {
+			const user = (await this.client.users.fetch(data[0], false)).tag;
+			return `• ${user}${''.padStart(40 - user.length, ' ')}:: ${data[1]} images`;
+		}));
 
 		await this.client.users.cache.get('296862433136476160').send(`Successfully sent message in ${eventChannel}`);
 
-		return eventChannel.send(`**Top Daily Posters in ${eventChannel}**\n\n*Total images posted: ${totalImages}*${codeBlock('asciidoc', sorted.join('\n'))}`)
+		return eventChannel.send(`**Top Daily Posters in ${eventChannel}**\n\n*Total images posted: ${this.lb.totalImages}*${codeBlock('asciidoc', sorted.join('\n'))}`)
 			.then(async (msg) => {
 				await this.client.settings.update('lastLeaderboardPost', msg.id);
-				messages.length = 0;
+				this.lb.messages = [msg];
+				this.lb.lastID = msg.id;
 				sorted.length = 0;
 			})
 			.catch(console.error);
+	}
+
+	_reset() {
+		this.lb = {
+			eventID: '',
+			lastID: '',
+			messages: [],
+			posters: {},
+			totalImages: 0
+		};
 	}
 
 };
