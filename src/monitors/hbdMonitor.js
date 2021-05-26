b31428329f954ad0858a3011b98778063e10b7c3
TheFloppyBanana <35372554+TheFloppyBanana@users.noreply.github.com>
2021-05-26 16:32:24 +0800

    fix: birthday monitor missing a 'hbd' check

--- src/monitors/hbdMonitor.js
+++ src/monitors/hbdMonitor.js
@@ -11,6 +11,7 @@ module.exports = class extends Monitor {
 	async run(message) {
 		if (!await message.hasAtLeastPermissionLevel(6)) return null;
 		if (!message.mentions.users.size) return null;
+		if (!message.content.startsWith('hbd')) return null;
 		return this.client.commands.get('birthday').run(message, message.mentions.users.first());
 	}
 
