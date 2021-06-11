b8f58fe61a2a4539691076b83f2bb8cadee903f1
TheFloppyBanana <35372554+TheFloppyBanana@users.noreply.github.com>
2021-06-11 11:27:04 +0800

    misc(eventMonitor): add other channels to react to

--- src/monitors/eventMonitor.js
+++ src/monitors/eventMonitor.js
@@ -16,14 +16,18 @@ module.exports = class extends Monitor {
 
 	constructor(...args) {
 		super(...args, {
-			ignoreOthers: false
+			ignoreOthers: false,
+			ignoreWebhooks: false
 		});
 	}
 
 	async run(message) {
-		if (!message.guild ||
-			message.guild.id !== '508495069914071040' ||
-			message.channel.id !== '815308859580874762') return null;
+		if (!message.guild && message.guild.id !== '508495069914071040') return null;
+
+		// #appeals, #server-suggestions, and #advisor-polls
+		if (['725353615304163328', '508873861979308032', '573221319441907732'].includes(message.channel.id)) return Promise.all(REACTIONS.map(async emote => await message.react(emote)));
+
+		if (message.channel.id !== '815308859580874762') return null;
 		if (!new RegExp(days.join('|')).test(message.content)) return null;
 		if (/\b(nsfw|dick|pussy|naked)\b|(loli|tit)/.test(message.content)) return message.react(no);
 		return Promise.all(REACTIONS.map(async emote => await message.react(emote)));
