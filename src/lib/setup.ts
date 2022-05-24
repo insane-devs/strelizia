fc61dd832e79f31f28030433084433b0a4ed4694
TheFloppyBanana <35372554+TheFloppyBanana@users.noreply.github.com>
2022-05-24 20:44:14 +0800

    update(setup): extend prisma to container, add env-plugin

--- src/lib/setup.ts
+++ src/lib/setup.ts
@@ -5,11 +5,24 @@ import 'reflect-metadata';
 import '@sapphire/plugin-logger/register';
 import '@sapphire/plugin-api/register';
 import '@sapphire/plugin-editable-commands/register';
+import '@kaname-png/plugin-env/register';
 import * as colorette from 'colorette';
 import { config } from 'dotenv-cra';
 import { join } from 'path';
 import { inspect } from 'util';
 import { srcDir } from './constants';
+import { container } from '@sapphire/framework';
+import { PrismaClient } from '@prisma/client';
+
+// Prisma
+container.prisma = new PrismaClient({
+	errorFormat: 'pretty',
+	log: [
+		{ emit: 'stdout', level: 'warn' },
+		{ emit: 'stdout', level: 'error' },
+		{ emit: 'event', level: 'query' }
+	]
+});
 
 // Read env var
 config({ path: join(srcDir, '.env') });
