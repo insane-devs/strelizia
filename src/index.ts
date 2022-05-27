37ab17852b1615ada5cfdbe72328c800c7a952b2
TheFloppyBanana <35372554+TheFloppyBanana@users.noreply.github.com>
2022-05-27 14:09:12 +0800

    src(index): move client options to extended client

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,7 @@
 import './lib/setup';
-import { LogLevel, SapphireClient } from '@sapphire/framework';
+import { StreliziaClient } from './lib/structures/StreliziaClient';
 
-const client = new SapphireClient({
-	defaultPrefix: 's/',
-	regexPrefix: /^(hey +)?strelizia[,! ]/i,
-	caseInsensitiveCommands: true,
-	logger: {
-		level: LogLevel.Debug
-	},
-	shards: 'auto',
-	intents: [
-		'GUILDS',
-		'GUILD_MEMBERS',
-		'GUILD_BANS',
-		'GUILD_EMOJIS_AND_STICKERS',
-		'GUILD_VOICE_STATES',
-		'GUILD_MESSAGES',
-		'GUILD_MESSAGE_REACTIONS',
-		'DIRECT_MESSAGES',
-		'DIRECT_MESSAGE_REACTIONS'
-	]
-});
+const client = new StreliziaClient();
 
 const main = async () => {
 	try {
